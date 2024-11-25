21fd9c1df4e56fb4ab3550146409ba9c8dc75c95
Minsol Park <mindol@kaist.ac.kr>
2024-11-25 19:33:58 +0900

    [BaseAviary] change camera distance

--- gym_pybullet_drones/envs/BaseAviary.py
+++ gym_pybullet_drones/envs/BaseAviary.py
@@ -150,8 +150,8 @@ class BaseAviary(gym.Env):
             self.CLIENT = p.connect(p.GUI) # p.connect(p.GUI, options="--opengl2")
             for i in [p.COV_ENABLE_RGB_BUFFER_PREVIEW, p.COV_ENABLE_DEPTH_BUFFER_PREVIEW, p.COV_ENABLE_SEGMENTATION_MARK_PREVIEW]:
                 p.configureDebugVisualizer(i, 0, physicsClientId=self.CLIENT)
-            p.resetDebugVisualizerCamera(cameraDistance=3,
-                                         cameraYaw=-30,
+            p.resetDebugVisualizerCamera(cameraDistance=0.5,
+                                         cameraYaw=-20,
                                          cameraPitch=-30,
                                          cameraTargetPosition=[0, 0, 0],
                                          physicsClientId=self.CLIENT
